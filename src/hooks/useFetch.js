cf4edf080a94628a2d78f4e127e7c8e018358a30
Arunim singhal <75484060+arunim-iiitlCS@users.noreply.github.com>
2022-11-30 10:19:22 +0530

    update

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,12 +4,10 @@ const useFetch = (file) => {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        fetch(`/database/${file}.json`)
+        fetch(`/Lucent-Biotech/database/${file}.json`)
         .then(res => res.json())
             .then(data => setData(data))
     },[file])
-
     return [data]
 }
-
 export default useFetch
